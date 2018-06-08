b71042a486e1021955a9bf62a9e5790a57d3a3e9
Alexander-Fortuin <fortxandi@gmail.com>
2018-06-08 13:24:22 +0200

    changed responsive view and gallery

---
+++ js/main.js
@@ -0,0 +1,125 @@
+'use strict'
+// Camping
+// image 1
+function toggle1() {
+  var click = document.querySelector('.click');
+  var over = document.querySelector('.box1');
+  if (over.className === 'box1 down') {
+    over.className = 'box1 up';
+  } else {
+    over.className = 'box1 down';
+  }
+}
+// image 2
+function toggle2() {
+  var click = document.querySelector('.click');
+  var over = document.querySelector('.box2');
+  if (over.className === 'box2 down') {
+    over.className = 'box2 up';
+  } else {
+    over.className = 'box2 down';
+  }
+}
+// image 3
+function toggle3() {
+  var click = document.querySelector('.click');
+  var over = document.querySelector('.box3');
+  if (over.className === 'box3 down') {
+    over.className = 'box3 up';
+  } else {
+    over.className = 'box3 down';
+  }
+}
+// Fishing
+// image 4
+function toggle4() {
+  var click = document.querySelector('.click');
+  var over = document.querySelector('.box4');
+  if (over.className === 'box4 down') {
+    over.className = 'box4 up';
+  } else {
+    over.className = 'box4 down';
+  }
+}
+// image 5
+function toggle5() {
+  var click = document.querySelector('.click');
+  var over = document.querySelector('.box5');
+  if (over.className === 'box5 down') {
+    over.className = 'box5 up';
+  } else {
+    over.className = 'box5 down';
+  }
+}
+// image 6
+function toggle6() {
+  var click = document.querySelector('.click');
+  var over = document.querySelector('.box6');
+  if (over.className === 'box6 down') {
+    over.className = 'box6 up';
+  } else {
+    over.className = 'box6 down';
+  }
+}
+// Hiking
+// image 7
+function toggle7() {
+  var click = document.querySelector('.click');
+  var over = document.querySelector('.box7');
+  if (over.className === 'box7 down') {
+    over.className = 'box7 up';
+  } else {
+    over.className = 'box7 down';
+  }
+}
+// image 8
+function toggle8() {
+  var click = document.querySelector('.click');
+  var over = document.querySelector('.box8');
+  if (over.className === 'box8 down') {
+    over.className = 'box8 up';
+  } else {
+    over.className = 'box8 down';
+  }
+}
+// image 9
+function toggle9() {
+  var click = document.querySelector('.click');
+  var over = document.querySelector('.box9');
+  if (over.className === 'box9 down') {
+    over.className = 'box9 up';
+  } else {
+    over.className = 'box9 down';
+  }
+}
+// Rock Climbing
+// image 10
+function toggle10() {
+  var click = document.querySelector('.click');
+  var over = document.querySelector('.box10');
+  if (over.className === 'box10 down') {
+    over.className = 'box10 up';
+  } else {
+    over.className = 'box10 down';
+  }
+}
+// image 11
+function toggle11() {
+  var click = document.querySelector('.click');
+  var over = document.querySelector('.box11');
+  if (over.className === 'box11 down') {
+    over.className = 'box11 up';
+  } else {
+    over.className = 'box11 down';
+  }
+}
+// image 12
+function toggle12() {
+  var click = document.querySelector('.click');
+  var over = document.querySelector('.box12');
+  if (over.className === 'box12 down') {
+    over.className = 'box12 up';
+  } else {
+    over.className = 'box12 down';
+  }
+}
